e20877546c56462489e5bc92a0025b7e5b6e786a
yousukeman3 <yousukeman3@gmail.com>
2016-05-29 14:20:17 +0900

    htydkyk

---
+++ WebSite5/JavaScript.js
@@ -0,0 +1,6 @@
+﻿document.querySelector("#button").addEventListener("click",
+    function () {
+        var b = new Number();
+        document.querySelector("#script1").innerHTML = "if(a==undefined){document.querySelector(\"#button\").removeEventListener(\"click\",function () {b=" + a + ";\n document.querySelector(\"#txt3\").value = b; });}document.querySelector(\"#button\").addEventListener(\"click\",function () {b=" + document.querySelector("#txt1").value + ";\n document.querySelector(\"#txt3\").value = b; });";
+        var a = Number(document.querySelector("#txt1").value);
+    });
